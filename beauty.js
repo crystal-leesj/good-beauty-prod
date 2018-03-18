7336717af3b815fe5783d5282db44438237a732e
Soo J Lee <soojlee@10-197-33-128.sds.oregonstate.edu>
2018-03-18 01:08:55 -0700

    initial commit

---
+++ beauty.js
@@ -0,0 +1,63 @@
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+
+    function getProducts(res, mysql, context, complete){
+        mysql.pool.query("SELECT products.id, name, brand, category, description, ingredient FROM products", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.products = results;
+            complete();
+        });
+    }
+
+    function getProduct(res, mysql, context, id, complete){
+        var sql = "SELECT id, name, brand, category, description, ingredient, comment FROM products WHERE id = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.product = results[0];
+            complete();
+        });
+    }
+
+    /*Display all people. Requires web based javascript to delete users with AJAX*/
+
+    router.get('/', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        // context.jsscripts = ["deleteperson.js"];
+        var mysql = req.app.get('mysql');
+        getProducts(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('products', context);
+            }
+
+        }
+    });
+
+    /* Display one person for the specific purpose of updating people */
+
+
+
+    /* Adds a person, redirects to the people page after adding */
+
+
+
+    /* The URI that update data is sent to in order to update a person */
+
+
+
+    /* Route to delete a person, simply returns a 202 upon success. Ajax will handle this. */
+
+
+
+    return router;
+}();
